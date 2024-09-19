f68a1e9a69c1180197d26006b18e491493a3fbac
Tanmay Khot <tnmykhot@gmail.com>
2024-09-19 00:26:36 -0700

    Added gohtml templates

--- main.go
+++ main.go
@@ -2,40 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("Parsing %s template: %v", filepath, err)
+		http.Error(w, "There was an error in parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing %s template: %v", filepath, err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1> Welcome to my page</h1>")
+	tplPath := "templates/home.gohtml"
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1> Contact page </h1> Email at <a href=\"mailto:tnmykhot@gmail.com\"> tnmykhot@gmail.com </a> to get in touch")
+	tplPath := "templates/contact.gohtml"
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-  <ul>
-	<li>
-	  <b>Questions 1?</b>
-	  Answer 1.
-	</li>
-	<li>
-	  <b>Question 2?</b>
-	  Answer 2
-	</li>
-	<li>
-	  <b>Question 3?</b>
-	  Answer 3
-	</li>
-  </ul>
-  `)
+	tplPath := "templates/faq.gohtml"
+	executeTemplate(w, tplPath)
 }
 
 func galleryIDHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +50,7 @@ func galleryIDHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	r := chi.NewRouter()
-	// r.Use(middleware.Logger) Use it across the app
+	// r.Use(middleware.Logger) // Use it across the app
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
